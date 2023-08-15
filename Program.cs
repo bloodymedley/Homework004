1d3bdd77870447668adcdf81ef8e240fe62ea4d3
bloodymedley <de4ster@gmail.com>
2023-08-15 18:45:40 +0300

    выгрузка репозитория

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+while (x<=n){
+    if( x % 2 == 0){
+
+    Console.Write(x + " ");}
+    x = x +1;
+}
